9a39f1c290c6bdd94ebd683dd776359e8cca1013
admin <542894924@qq.com>
2025-10-30 17:29:03 +0800

    优化页面和产品配置

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -33,7 +33,7 @@ class MainWindow(QMainWindow):
         self.enable_interface_auto = self.config.get("features", {}).get("interface_automation", False)
 
         self.setWindowTitle("测试工具")
-        self.setGeometry(100, 100, 1600, 800)
+        self.setGeometry(100, 100, 1800, 800)
 
         # 设置窗口图标
         self.setWindowIcon(self.create_icon())
